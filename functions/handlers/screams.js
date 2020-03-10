1473da06866a3dd4745ab9e90b5b3a247ff60de1
See Yang <seeyang24@gmail.com>
2020-03-09 20:30:17 -0500

    created like/unlike routes and delete a scream route

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -102,7 +102,8 @@ exports.commentOnScream = (request, response) => {
       if (!doc.exists) {
         return res.status(404).json({ error: 'Scream not found' });
       }
-    //   return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
+      // update the comment count by 1
+      return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
     })
     .then(() => {
       return db.collection('comments').add(newComment);
@@ -116,3 +117,119 @@ exports.commentOnScream = (request, response) => {
       response.status(500).json({ error: 'Something went wrong' });
     });
 }; // end commonOnScream
+
+// begin likeScream
+exports.likeScream = (request, response) => {
+    // limit a user to like a particular scream ID to 1 like
+    const likeDocument = db.collection('likes').where('userHandle', '==', request.user.handle)
+        .where('screamId', '==', request.params.screamId).limit(1);
+
+    const screamDocument = db.doc(`/screams/${request.params.screamId}`);
+
+    let screamData;
+    // check if this scream ecists
+    screamDocument.get()
+        .then(doc => {
+            if(doc.exists) {
+                screamData = doc.data();
+                screamData.screamId = doc.id; // assign the screamId
+                return likeDocument.get();
+            } else {
+                // if scream doesn't exist
+                return response.status(404).json({error: 'Scream not found.'});
+            }
+        })
+        .then(data => {
+            // if there is no like, add the like and pass in the screamId and User Handle
+            if (data.empty) {
+                return db.collection('likes').add({
+                    screamId: request.params.screamId,
+                    userHandle: request.user.handle
+                })
+                .then(() => {
+                    screamData.likeCount++; // increment the like count by 1
+                    return screamDocument.update({likeCount: screamData.likeCount})  // increment the like count in the document of the scream in the database
+                })
+                .then(() => {
+                    return response.json(screamData); // return the screamData in JSON format
+                })
+            } else {
+                // if we do have a like already
+                return response.status(400).json({ error: 'Scream already liked.'});
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            response.status(500).json({error: error.code});
+        })
+}; // end likeScream
+
+// begin UnlikeScream
+exports.unlikeScream = (request, response) => {
+    const likeDocument = db.collection('likes').where('userHandle', '==', request.user.handle)
+        .where('screamId', '==', request.params.screamId).limit(1);
+
+    const screamDocument = db.doc(`/screams/${request.params.screamId}`);
+
+    let screamData;
+    // check if this scream ecists
+    screamDocument.get()
+        .then(doc => {
+            if(doc.exists) {
+                screamData = doc.data();
+                screamData.screamId = doc.id; // assign the screamId
+                return likeDocument.get();
+            } else {
+                // if scream doesn't exist
+                return response.status(404).json({error: 'Scream not found.'});
+            }
+        })
+        .then(data => {
+            // if there is not like, show this error
+            if (data.empty) {
+                return response.status(400).json({ error: 'Scream not liked.'});
+            } else {
+                // if we do have a like already
+                return db
+                .doc(`/likes/${data.docs[0].id}`)
+                .delete()
+                .then(() => {
+                  screamData.likeCount--;
+                  return screamDocument.update({ likeCount: screamData.likeCount });
+                })
+                .then(() => {
+                  res.json(screamData);
+                });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            response.status(500).json({error: error.code});
+        });
+}; // end unlikeScream
+
+// Begin Delete Scream
+exports.deleteScream = (request, response) => {
+    const document = db.doc(`/screams/${request.params.screamId}`);
+    document.get()
+        .then(doc => {
+            // if the document does not exist
+            if (!doc.exists) {
+                return response.status(404).json({error: 'Scream not found.'});
+            }
+            // check to see if the userhandle is the correct user handle to that scream
+            if (doc.data().userHandle !== request.user.handle) {
+                // 403 = unauthorized error
+                return response.status(403).json({error: 'Unauthorized.'});
+            } else {
+                return document.delete();
+            }
+        })
+        .then(() => {
+            response.json({message: 'Scream deleted successfully.'});
+        })
+        .catch(error => {
+            console.error(error);
+            response.status(500).json({error: error.code});
+        });
+}; // end deleteScream
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
 // imports
-const {getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream} = require('./handlers/screams');
+const {getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream} = require('./handlers/screams');
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
 const FBAuth = require('./util/fbAuth');
 
@@ -13,9 +13,9 @@ app.get('/screams', getAllScreams); // get all screams
 app.post('/scream', FBAuth, postOneScream); // // create a single scream
 app.get('/scream/:screamId', getScream); // a route parameter with the screamID
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream); // post a comment on a specific Scream
-// app.get('/scream/:screamId/like', FBAuth, likeScream); // like a scream
-// app.get('/scream/:screamId/unlike', FBAuth, unlikeScream); // unlike a scream
-// TO DO - delete a scream
+app.get('/scream/:screamId/like', FBAuth, likeScream); // like a scream
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream); // unlike a scream
+app.delete('/scream/:screamId', FBAuth, deleteScream); // TO DO - delete a scream
 
 // Signup Route and Login Route - USERS ROUTE
 app.post('/signup', signup);
